39808590bcd17b0dc5158fa3325ed6ae784ac74a
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-02-25 15:53:30 +0300

    Initial commit

---
+++ src/main/java/ru/gb/lessons/App.java
@@ -0,0 +1,14 @@
+package ru.gb.lessons;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+
+        System.out.println( "Hello World!" );
+    }
+}

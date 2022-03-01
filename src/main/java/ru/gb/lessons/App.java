1b5c6e3a070250ef96d9e735ba2067c05f862078
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-03-01 16:35:31 +0300

    lesson2

--- src/main/java/ru/gb/lessons/App.java
+++ src/main/java/ru/gb/lessons/App.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 /**
  *
@@ -19,11 +20,15 @@ public class App {
 
         webDriver.manage().window().setSize(new Dimension(1500, 1100));             //настроить размеры окна браузера
         //webDriver.findElement(By.xpath(""));                                                  //метод для поиска элемента (findElements  - элементов)
-        //webDriver.findElement(By.id(""));                                                      //метод для поиска элемента по id
+        //webDriver.findElement(By.id(""));                                                     //метод для поиска элемента по id
         webDriver.findElement(By.xpath                                                          //clear - почистить;.click-кликнуть по элементу;.sendKeys-вписать текст в элемент;.getText -получить текст;.getLocation- получить локацию; .findElement - получить другой элемент из элемента; .getAttribute - получить атрибут;
                 ("//div[@class='signin-link']//span[@class='signin-link__title']"))
                 .click();
-        Thread.sleep(6000);                                                               //остановка на 10 секунд проверить откроется ли страница
+        WebElement modal__login = webDriver.findElement(By.xpath                                //выносим авторизацию в отдельную переменную, что бы проще было искать элементы
+                ("//div[@class='modal__login']"));                                 //WebElement modal__login = webDriver.findElement(By.xpath).var
+        modal__login.findElement(By.name("email")).sendKeys("laserjet007@rambler.ru");//ищем внутри созданной переменной, найти элемент по имени. sendKeys -ввод текста
+
+                Thread.sleep(6000);                                                               //остановка на 10 секунд проверить откроется ли страница
         webDriver.quit();                                                                       //закрываем webdriver
 
     }

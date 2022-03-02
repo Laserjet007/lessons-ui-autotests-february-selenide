3f07fda11319cf273c0cde3b70e930e5ba0f0e3f
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-03-02 13:10:13 +0300

    lesson2

--- src/main/java/ru/gb/lessons/App.java
+++ src/main/java/ru/gb/lessons/App.java
@@ -6,6 +6,11 @@ import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
 /**
  *
  */
@@ -17,18 +22,29 @@ public class App {
         WebDriver webDriver = WebDriverManager.chromedriver().create();                         //safaridriver().create(); упрощаем создание веб драйва добавляя в мавин депенденси <!-- https://mvnrepository.com/artifact/io.github.bonigarcia/webdrivermanager -->//<dependency>//    <groupId>io.github.bonigarcia</groupId>//    <artifactId>webdrivermanager</artifactId>//    <version>5.1.0</version>//</dependency>
 
         webDriver.get("https://www.shatura.com/");                                              //в сценарии пишем страницу куда нужно перейти
-
         webDriver.manage().window().setSize(new Dimension(1500, 1100));             //настроить размеры окна браузера
+        webDriver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);                    //неявное ожидание - по дефолту каждого действия, ждать элемент
         //webDriver.findElement(By.xpath(""));                                                  //метод для поиска элемента (findElements  - элементов)
         //webDriver.findElement(By.id(""));                                                     //метод для поиска элемента по id
         webDriver.findElement(By.xpath                                                          //clear - почистить;.click-кликнуть по элементу;.sendKeys-вписать текст в элемент;.getText -получить текст;.getLocation- получить локацию; .findElement - получить другой элемент из элемента; .getAttribute - получить атрибут;
                 ("//div[@class='signin-link']//span[@class='signin-link__title']"))
                 .click();
-        WebElement modal__login = webDriver.findElement(By.xpath                                //выносим авторизацию в отдельную переменную, что бы проще было искать элементы
-                ("//div[@class='modal__login']"));                                 //WebElement modal__login = webDriver.findElement(By.xpath).var
-        modal__login.findElement(By.name("email")).sendKeys("laserjet007@rambler.ru");//ищем внутри созданной переменной, найти элемент по имени. sendKeys -ввод текста
+        WebElement modal__login = webDriver.findElement(By.xpath                               //выносим авторизацию в отдельную переменную, что бы проще было искать элементы
+                ("//div[@class='modal__login']"));                                            //WebElement modal__login = webDriver.findElement(By.xpath).var
 
-                Thread.sleep(6000);                                                               //остановка на 10 секунд проверить откроется ли страница
+        //By authModal__Login_Locator = By.xpath("//div[@class='modal__login']");               //создали патч что бы вставить в следующую строку
+        //new WebDriverWait(webDriver,5).until(                                                 //остановка в случае ошибки, для возможности открытия (до нажатия клика). тайм аут 5 сек в течении которых идут попытки дождаться действия через каждые 5 милисек. дальше указываем элемент который нужно обнаружить. ExpectedConditions. elementToBeClickable -ждять пока элемент станет кликабельным, yextToBe - пока не появится текст,
+        //      ExpectedConditions.presenceOfElementLocated(authModal__Login_Locator));         //эта конструкция называется - явное ожидание
+        //new WebDriverWait(webDriver,5,500).until(                                             //ожидание , когда элемент станет видимым (именно элемент, а не как в предидущей строке - локатор)
+        //      ExpectedConditions.visibilityOf(webDriver.findElement(By.xpath("//div[@class='modal__login']"))));
+        modal__login.findElement(By.name("email")).sendKeys("laserjet007@rambler.ru");//ищем внутри созданной переменной, найти элемент по имени. sendKeys -ввод текста
+        modal__login.findElement(By.name("password")).sendKeys("999999999");          //то же самое с вводом пароля
+        modal__login.findElement(By.className("btn__txt")).click();
+        webDriver.findElement(By.xpath("//div[@class='signin-link']//span[@class='signin-link__title']")).click();
+        webDriver.findElement(By.xpath("//div[@class='dropdown-menu show']//a[text()='Выйти']")).click();
+
+        //modal__login.findElement(By.cssSelector("div.header_user")).click();                  //поиск по css селектору
+               // Thread.sleep(36000);                                                          //остановка на 10 секунд проверить откроется ли страница
         webDriver.quit();                                                                       //закрываем webdriver
 
     }

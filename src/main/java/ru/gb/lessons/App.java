ef903852b227e22aa7b22182231350486132eba7
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-03-03 11:16:20 +0300

    lesson2

--- src/main/java/ru/gb/lessons/App.java
+++ src/main/java/ru/gb/lessons/App.java
@@ -3,6 +3,7 @@ package ru.gb.lessons;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -16,8 +17,10 @@ public class App {
 /**    System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");     говорим библиотеке webdriver где хранится chromdriver, который запустит браузер
 *      WebDriver webDriver = new ChromeDriver();                                                 создаем класс с которым будем работать
 */
-        WebDriver webDriver = WebDriverManager.chromedriver().create();                         //safaridriver().create(); упрощаем создание веб драйва добавляя в мавин депенденси <!-- https://mvnrepository.com/artifact/io.github.bonigarcia/webdrivermanager -->//<dependency>//    <groupId>io.github.bonigarcia</groupId>//    <artifactId>webdrivermanager</artifactId>//    <version>5.1.0</version>//</dependency>
-
+        ChromeOptions chromeOptions = new ChromeOptions();                                      //создаем переменную куда добавим условие capabilities() - не загружать фото сайта во время теста для ускорения
+        chromeOptions.addArguments("--blink-settings=imagesEnabled=false");                     //создаем аргумент для capabilities, который будет блокировать загрузку фото
+        WebDriver webDriver = WebDriverManager.chromedriver().                                  //safaridriver().create(); упрощаем создание веб драйва добавляя в мавин депенденси <!-- https://mvnrepository.com/artifact/io.github.bonigarcia/webdrivermanager -->//<dependency>//    <groupId>io.github.bonigarcia</groupId>//    <artifactId>webdrivermanager</artifactId>//    <version>5.1.0</version>//</dependency>
+                capabilities(chromeOptions).create();                                           //capabilities() добавляем chromeOptions что бы исключить загрузку фото при тестировании
         webDriver.get("https://www.shatura.com/");                                              //в сценарии пишем страницу куда нужно перейти
         webDriver.manage().window().setSize(new Dimension(1500, 1100));             //настроить размеры окна браузера
         webDriver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);                    //неявное ожидание - по дефолту каждого действия, ждать элемент
@@ -37,6 +40,7 @@ public class App {
         modal__login.findElement(By.name("email")).sendKeys("laserjet007@rambler.ru");//ищем внутри созданной переменной, найти элемент по имени. sendKeys -ввод текста
         modal__login.findElement(By.name("password")).sendKeys("999999999");       //то же самое с вводом пароля
         modal__login.findElement(By.className("btn__txt")).click();
+        //modal__login.findElement(By.xpath(".//button[span[text()='Войти']]")).click           .// - точка означает найти элемент в созданной в идеи мной - классе элементов modal__login.)
         webDriver.findElement(By.xpath("//div[@class='signin-link']//span[@class='signin-link__title']")).click();
         //new WebDriverWait(webDriver,5).until(                                                 //подождать когда исчезнет элемент  в случае если он висит
         //        ExpectedConditions.invisibilityOf(modal__login));

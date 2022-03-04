0e9d1262069d994cd3a66d97a6c6c59a22928158
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-03-04 11:18:00 +0300

    lesson3

--- src/main/java/ru/gb/lessons/AddProductToCart.java
+++ src/main/java/ru/gb/lessons/AddProductToCart.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 
@@ -17,15 +18,13 @@ public class AddProductToCart {
         webDriver.get("https://www.shatura.com/");
         webDriver.manage().window().setSize(new Dimension(1500, 1100));
         webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //new Actions(webDriver)                                                              //способ накидать каких - либо действий (в данном случае на локатор билдер)
+        //        .moveToElement(webDriver.findElement(By.xpath("//div[@class='catalog-nav__section-list']//span[@class='catalog-nav__section-list-li-title']")))  //взять элемент
+        //        .clickAndHold()                                                           //сказать что хотим его нажать.  либо использовать трекинтроп
+        //        .build()                                                                    // в нашем билдере
+        //       .perform();                                                                 //выполнить
 
-        new Actions(webDriver)                                                              //способ накидать каких - либо действий (в данном случае на локатор билдер)
-                .moveToElement(webDriver.findElement(By.xpath("//div[@class='catalog-nav__section-list']//span[@class='catalog-nav__section-list-li-title']")))  //взять элемент
-                .clickAndHold()                                                           //сказать что хотим его нажать.  либо использовать трекинтроп
-                .build()                                                                    // в нашем билдере
-                .perform();                                                                 //выполнить
-
-        webDriver.findElement(By.xpath("")).click();
-
+        webDriver.findElement(By.xpath("//div[@class='catalog-nav__section-list']//span[@class='catalog-nav__section-list-li-title']")).click();
 
 
         Thread.sleep(36000);

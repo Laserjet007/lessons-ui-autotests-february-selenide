02a1473c1f20bf8963d0e8d00c26fb7de9799434
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-03-09 14:31:42 +0300

    lesson4

---
+++ src/main/java/ru/gb/lessons/lesson_4/Triangle.java
@@ -0,0 +1,11 @@
+package ru.gb.lessons.lesson_4;
+
+public class Triangle {
+    private int a;                                                //стороны
+    private int b;
+    private int c;
+
+    public int countPerimeter() {                                  // метод
+
+    }
+}

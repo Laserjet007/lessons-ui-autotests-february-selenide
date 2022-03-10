546fde61547416537b43e3fab1e9e147acc87a0d
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-03-10 12:58:53 +0300

    lesson4

--- src/main/java/ru/gb/lessons/lesson_4/Triangle.java
+++ src/main/java/ru/gb/lessons/lesson_4/Triangle.java
@@ -1,11 +1,33 @@
 package ru.gb.lessons.lesson_4;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data                                                             //добавляем из библиотеки lombok вместо прописывания переменных
+@AllArgsConstructor                                               //если требуется указать все параметры конструктора
+//@NoArgsConstructor                                              // случае , если нет аргументов
 public class Triangle {
+//  @Getter                                                       //добавляем из библиотеки lombok вместо переменных если не проставить @Data над методом
+//  @Setter
     private int a;                                                //стороны
     private int b;
     private int c;
 
+    private void checkSidesArePositive(){                         //пишем отдельный метод
+        if (a <= 0 || b <= 0 || c <= 0) {                         //где указываем что a <= 0 или b <= 0 или c <= 0
+            throw new IllegalArgumentException("Sides must be positive");  //то в этом случае кидаем новую ошибку
+        }
+    }
+//проверка на невыраженность:
+    private void checkOneSideIsSmallerThanSumOfOthers(){           //пишем отдельный метод
+         if (a + b <= c || b + c <= a || c + a <= b) {
+             throw new IllegalArgumentException("One side can' be greater than sum of others");  //то в этом случае кидаем новую ошибку
+    }
+}
+    // TODO: 09.03.2022 реализовать метод
     public int countPerimeter() {                                  // метод
-
+        checkSidesArePositive();                                   //проверяем ошибку из конструктора выше
+        checkOneSideIsSmallerThanSumOfOthers();
+             return a + b + c;                                     // с начала возвращаем 0 - не пишем реализацию
     }
 }
---
+++ src/test/java/ru/gb/lessons/TriangleTest.java
@@ -0,0 +1,103 @@
+package ru.gb.lessons;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.gb.lessons.lesson_4.Triangle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * Unit test for simple LoginAndLogout.
+ */
+public class TriangleTest {
+    @Test
+    @DisplayName("Периметр треугольника 3, 4, 5 должен быть равен 12")   //название теста
+    public void countPerimeterEgyptTriangleSuccessfulTest() {      //указываем метод, который хотим протестировать. countPerimeterEgyptTriangleSuccessfulTest - площадь периметра... - так писать не обязательно, зависит от проэкта
+       // new Actions(new ChromeDriver());                         заглушка до написания самих тестов, что бы код не ломался
+        // assertTrue( true );                                     //assert - проверяет условия
+//реализация теста: - первая составляющая из трех - Arrange:
+        Triangle triangle = new Triangle(3, 4, 5);        //готовим данные
+        int perimeter = triangle.countPerimeter();                 //следующая часть - Act:
+        assertEquals(12,perimeter);                        //третья часть - Assert:
+    }
+//unit tests - положительные: первые три теста почти одинаковые (отличаются только входные данные и ожидаемые результаты)
+    @Test
+    @DisplayName("Периметр треугольника 3, 4, 6 должен быть равен 12")
+    public void countPerimeterGreater90TriangleSuccessfulTest() {
+        Triangle triangle = new Triangle(3, 4, 6);
+        int perimeter = triangle.countPerimeter();
+        assertEquals(13,perimeter);
+    }
+    @Test
+    @DisplayName("Периметр треугольника 3, 3, 3 должен быть равен 9")
+    public void countPerimeterWithEqualSidesTriangleSuccessfulTest() {
+        Triangle triangle = new Triangle(3, 3, 3);
+        int perimeter = triangle.countPerimeter();
+        assertEquals(9,perimeter);
+    }
+//unit tests -  отрицательные:
+    @Test
+    @DisplayName("Периметр треугольника негатив сценарий 1")
+    public void countPerimeter1sideIsZeroFailedTest() {
+        Triangle triangle = new Triangle(0, 3, 3);
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, triangle::countPerimeter);  //понять, что есть ошибка - пишем какую ошибку ожидаем
+        assertEquals("Sides must be positive",illegalArgumentException.getMessage());                                        //проверить текст
+    }
+    @Test
+    @DisplayName("Периметр треугольника негатив сценарий 2")
+    public void countPerimeter2sideIsZeroFailedTest() {
+        Triangle triangle = new Triangle(1, 0, 2);
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, triangle::countPerimeter);  //понять, что есть ошибка - пишем какую ошибку ожидаем
+        assertEquals("Sides must be positive", illegalArgumentException.getMessage());
+    }
+    @Test
+    @DisplayName("Периметр треугольника негатив сценарий 3")
+    public void countPerimeter3sideIsZeroFailedTest() {
+         Triangle triangle = new Triangle(1, 1, 0);
+         IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, triangle::countPerimeter);  //понять, что есть ошибка - пишем какую ошибку ожидаем
+         assertEquals("Sides must be positive", illegalArgumentException.getMessage());
+
+        }
+//unit tests - отрицательные что и с другими сторонами так же все будет работать:
+    @Test
+    @DisplayName("Периметр треугольника негатив сценарий 1")
+    public void countPerimeter1sideIsNegativeFailedTest() {
+        Triangle triangle = new Triangle(-3, 3, 3);
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, triangle::countPerimeter);  //понять, что есть ошибка - пишем какую ошибку ожидаем
+        assertEquals("Sides must be positive",illegalArgumentException.getMessage());                                        //проверить текст
+    }
+    @Test
+    @DisplayName("Периметр треугольника негатив сценарий 2")
+    public void countPerimeter2sideIsNegativeFailedTest() {
+        Triangle triangle = new Triangle(1, -1, 1);
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, triangle::countPerimeter);  //понять, что есть ошибка - пишем какую ошибку ожидаем
+        assertEquals("Sides must be positive", illegalArgumentException.getMessage());
+    }
+    @Test
+    @DisplayName("Периметр треугольника негатив сценарий 3")
+    public void countPerimeter3sideIsNegativeFailedTest() {
+        Triangle triangle = new Triangle(1, 1, -1);
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, triangle::countPerimeter);  //понять, что есть ошибка - пишем какую ошибку ожидаем
+        assertEquals("Sides must be positive", illegalArgumentException.getMessage());
+    }
+//проверка правильности получившихся сторон: одна сторона не может быть ровна сумме двух других
+    @Test
+    @DisplayName("проверка правильности получившихся сторон:")
+    public void countPerimeterInvalidTriangleFailedTest() {
+        Triangle triangle = new Triangle(1, 1, 6);
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, triangle::countPerimeter);  //понять, что есть ошибка - пишем какую ошибку ожидаем
+        assertEquals("One side can' be greater than sum of others", illegalArgumentException.getMessage());
+    }
+//проверка для других сторон
+    @Test
+    @DisplayName("проверка правильности получившихся других сторон:")
+    public void countPerimeterInvalidTriangle2FailedTest() {
+        Triangle triangle = new Triangle(1, 2, 1);
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, triangle::countPerimeter);  //понять, что есть ошибка - пишем какую ошибку ожидаем
+        assertEquals("One side can' be greater than sum of others", illegalArgumentException.getMessage());
+    }
+
+
+}

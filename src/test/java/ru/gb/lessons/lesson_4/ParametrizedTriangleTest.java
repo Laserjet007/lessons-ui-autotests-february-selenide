9e68de101c15cb72ed854a9df3b977c54dbf754d
Laserjet007 <93554470+Laserjet007@users.noreply.github.com>
2022-03-21 10:56:45 +0300

    lesson5

--- src/test/java/ru/gb/lessons/lesson_4/ParametrizedTriangleTest.java
+++ src/test/java/ru/gb/lessons/lesson_4/ParametrizedTriangleTest.java
@@ -2,11 +2,10 @@ package ru.gb.lessons;
 
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.Extensions;
-import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 import ru.gb.lessons.lesson_4.Colour;
+import ru.gb.lessons.lesson_4.TimerTestExtension;
 import ru.gb.lessons.lesson_4.Triangle;
 
 import java.util.stream.Stream;
@@ -14,7 +13,7 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@ExtendWith(BeforeAllExtension.class)  //создаем класс BeforeAllExtension
+@ExtendWith(TimerTestExtension.class)  //создаем класс BeforeAllExtension
 public class ParametrizedTriangleTest {
     @BeforeEach                                                                    //метод beforeEach, который готовит данные для всего раздела тестов
     void setUp(){
